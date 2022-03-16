6e5c85a98bce998908399eaec0a194ace3feb8ed
Muharrem ÖZER <86782430+ozermuharrem@users.noreply.github.com>
2022-03-16 05:25:41 +0300

    Add files via upload

--- ozermuharrem.com/asil_site/projeler/projeler.js
+++ ozermuharrem.com/asil_site/projeler/projeler.js
@@ -1,7 +1,7 @@
 
 
-var tarih = new Date(2022,02,12);
+var projeTarih = new Date(2022,02,12);
 
-document.getElementById('time').textContent =tarih.getDate()+"/"+(tarih.getMonth()+1)+"/"+tarih.getFullYear();
+document.getElementById('time').textContent =projeTarih.getDate()+"/"+(projeTarih.getMonth()+1)+"/"+tarih.getFullYear();
 
-document.getElementById('time1').textContent =tarih.getDate()+"/"+(tarih.getMonth()+1)+"/"+tarih.getFullYear();
+document.getElementById('time1').textContent =projeTarih.getDate()+"/"+(projeTarih.getMonth()+1)+"/"+projeTarih.getFullYear();
